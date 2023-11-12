765e1652615cfb5aa86310e85a7d828e253e78ca
sburke781 <sburke781@gmail.com>
2023-11-12 11:00:57 +1100

    Remove App Version No, Cleanup and rename driver

--- hubitat/MC_MobileDevice_Driver.groovy
+++ hubitat/MC_MobileDevice_Driver.groovy
@@ -22,11 +22,11 @@
 public static String mcURLPath() { return "mc"; }
 
 metadata {
-	definition (name: 'MC Mobile Device', namespace: 'simnet', author: 'Simon Burke') {
+	definition (name: 'Mobile Device', namespace: 'simnet', author: 'Simon Burke') {
         
         capability 'Battery'
-        capability 'MotionSensor'
-        capability 'PresenceSensor'
+        //capability 'MotionSensor'
+        //capability 'PresenceSensor'
         capability 'AudioVolume'
         capability 'Notification'
         capability 'Configuration'
@@ -51,12 +51,14 @@ metadata {
         attribute 'switch', 'ENUM ["on", "off"]'
         
         //Network Attributes
-        attribute 'wifiNetwork', 'string' // Wi-Fi Netowrk SSID the mobile device is currently connected to
+        //attribute 'wifiNetwork', 'string' // Wi-Fi Netowrk SSID the mobile device is currently connected to
         
         //Call and Message Attributes
+        /*
         attribute 'callStatus', 'ENUM["InCall", "Idle", "unknown"]'
         attribute 'missedCall', 'ENUM["yes", "no", "unknown"]'
         attribute 'unreadMessage', 'ENUM["yes", "no", "unknown"]'
+        */
         
         // General Attributes
         attribute 'lastUpdated', 'date'
@@ -67,12 +69,12 @@ metadata {
         command 'setBattery', [[name:'batteryReading', type: 'NUMBER', description: 'Enter the new battery reading (%)' ] ]
         
         //Motion Sensor Capability Commands
-        command 'active'
-        command 'inactive'
+        //command 'active'
+        //command 'inactive'
         
         //Presence Sensor Commands (not part of the capability)
-        command 'present'
-        command 'notPresent'
+        //command 'present'
+        //command 'notPresent'
         
         // Audio Volume Capability Commands
         command 'mute'
@@ -106,16 +108,6 @@ metadata {
         command 'deviceHeartbeat'
     }    
     
-    /*
-  preferences {
-    
-    // Logging Preferences
-    input(name: "DebugLogging", type: "bool", title:"Enable Debug Logging",                   displayDuringSetup: true, defaultValue: false)
-    input(name: "WarnLogging",  type: "bool", title:"Enable Warning Logging",                 displayDuringSetup: true, defaultValue: true )
-    input(name: "ErrorLogging", type: "bool", title:"Enable Error Logging",                   displayDuringSetup: true, defaultValue: true )
-    input(name: "InfoLogging",  type: "bool", title:"Enable Description Text (Info) Logging", displayDuringSetup: true, defaultValue: false)
-  }
-	*/
     preferences {
     
       // Platform and authentication Preferences
@@ -133,8 +125,8 @@ metadata {
       input(name: "CloudComm", type: "bool", title:"Use Cloud Communications?", description: "Turn on to use cloud communications back to HE when not on Wi-Fi", displayDuringSetup: true, defaultValue: false)
       
       input(name: "SyncHEMode", type: "bool", title:"Sync HE Mode?", description: "Turn on to send HE mode updates to the mobile device", displayDuringSetup: true, defaultValue: false)
-      input(name: "TrackWIFINetwork", type: "bool", title:"Track Mobile Device Wi-Fi Network?", description: "Record Wi-Fi Network SSID of the mobile device (only whitelisted SSID's)", displayDuringSetup: true, defaultValue: false)
-      input(name: "WIFINetWhitelist", type: "string", title:"Wi-Fi Network SSID Whitelist", description: "List of Wi-Fi Network SSID's to track, separated by a /", displayDuringSetup: true, defaultValue: "")
+      //input(name: "TrackWIFINetwork", type: "bool", title:"Track Mobile Device Wi-Fi Network?", description: "Record Wi-Fi Network SSID of the mobile device (only whitelisted SSID's)", displayDuringSetup: true, defaultValue: false)
+      //input(name: "WIFINetWhitelist", type: "string", title:"Wi-Fi Network SSID Whitelist", description: "List of Wi-Fi Network SSID's to track, separated by a /", displayDuringSetup: true, defaultValue: "")
       //input(name: "TrackCallStatus", type: "bool", title:"Track Mobile Device Call Status?", description: "Turn on to track whether the mobile device is in a call or not", displayDuringSetup: true, defaultValue: false)
       //input(name: "TrackMissedCalls", type: "bool", title:"Track Mobile Device Missed Calls?", description: "Turn on to track whether the mobile device has any missed calls", displayDuringSetup: true, defaultValue: false)
       
@@ -152,12 +144,13 @@ void initialized() {
   //sendEvent(name: "batteryStatus", value: "Idle");
 }
 
+void updated() {
+    //Use library method to set schedule to disable debug logging if turned on
+    updated_debugTimout();
+}
+
 //Configure Method
 void configure() {
-    debugLog("Cloud URI: ${parent.getCloudUri()}");
-    debugLog("Local URI: ${parent.getLocalUri()}");
-    debugLog("Access Token: ${parent.state.accessToken}");
-    debugLog("DeviceId: ${device.deviceNetworkId}");
     
     if(CommandMethod == "Tasker"){ sendTaskerCommand("configuration/hecloudendpoint", parent.getCloudUri()); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("mc_he_cloud_api_endpoint", parent.getCloudUri()); }
@@ -170,12 +163,14 @@ void configure() {
 }
 
 //Motion Sensor Methods
+/*
 void active() {
     sendEvent(name: 'motion', value: 'active');
     debugLog('active: Device reported as being active');
 
     setLastUpdate();
 }
+
 
 void inactive() {
     sendEvent(name: 'motion', value: 'inactive');
@@ -198,6 +193,7 @@ void notPresent() {
 
     setLastUpdate();
 }
+*/
 
 void syncMode(String newMode) {
     if(SyncHEMode) {
@@ -250,8 +246,7 @@ void setBrightness(Number brightnessVal) {
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("setBrightness", "${brightnessVal}"); }
     sendEvent(name: 'brightness', value: brightnessVal);
     
-    infoLog("setBrightness: Device brightness updated, new value = ${brightnessVal}");
-    
+    infoLog("Device brightness updated to ${brightnessVal}");
     setLastUpdate();
 }
 
@@ -259,81 +254,123 @@ void setBrightness(Number brightnessVal) {
 void mute() {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("volume/media/mute", ""); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("mediaVolumeMute", ""); }
+    
+    infoLog("Device has been muted");
+    setLastUpdate();
 }
 
 void unmute() {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("volume/media/unmute", ""); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("mediaVolumeUnmute", ""); }
+    
+    infoLog("Device has been unmuted");
+    setLastUpdate();
 }
 
 void setVolume(volumelevel) { 
     if(CommandMethod == "Tasker"){ sendTaskerCommand("volume/media/set", "${volumelevel}"); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("setMediaVolume", "${volumelevel}"); }
+    
+    infoLog("Device media volume has been adjusted to ${volumelevel}");
+    setLastUpdate();
 }
 
 void volumeDown() {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("volume/media/down", ""); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("mediaVolumeDown", ""); }
+    
+    infoLog("Device media volume has been turned down");
+    setLastUpdate();
 }
 
 void volumeUp() { 
     if(CommandMethod == "Tasker"){ sendTaskerCommand("volume/media/up", ""); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("mediaVolumeUp", ""); }
+    
+    infoLog("Device media volume has been turned up");
+    setLastUpdate();
 }
 
 //Notification Methods
 void deviceNotification(String text) {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("notifications/notify", "Mobile Controller||${text}"); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("notify", "Mobile Controller||${text}"); }
+    
+    infoLog("Device notification has been sent");
+    debugLog("deviceNotification: Notification text = ${text}");
+    setLastUpdate();
 }
 
 void customDeviceNotification(String title, String text){
     if(CommandMethod == "Tasker"){ sendTaskerCommand("notifications/notify", "${title}||${text}"); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("notify", "${title}||${text}"); }
+    
+    infoLog("Device custom notification has been sent");
+    debugLog("customDeviceNotification: Notification title = ${title}, text = ${text}");
+    setLastUpdate();
 }
 
 void cancelNotification(String title) {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("notifications/cancel", title); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("cancelNotification", title); }
+    
+    infoLog("Device notification has been cancelled");
+    debugLog("cancelNotification: Notification cancelled with title = ${title}");
+    setLastUpdate();
 }
 
 //Switch Methods
+/*
 void on() {
     sendEvent(name: 'switch', value: 'On');
-    infoLog("on: mobile device turned on");
-    
+
+    infoLog("on: Device turned on");    
     setLastUpdate();
 }
 
 void off() {
     sendEvent(name: 'switch', value: 'Off');
-    infoLog("off: mobile device turned off");
-    
+
+    infoLog("off: Device turned off");
     setLastUpdate();
 }
+*/
 
-//Custom Switch Methods
+//Custom Screen Methods
 void turnScreenOn() {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("screen/on", ""); }
     if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("screenOn", ""); }
+    
+    infoLog("Device screen has been turned on");
+    setLastUpdate();
 }
 
 void turnScreenOff() {
     if(CommandMethod == "Tasker"){ sendTaskerCommand("screen/off", ""); }
-    if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("screenOff", ""); }    
+    if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("screenOff", ""); }
+    
+    infoLog("Device screen has been turned off");
+    setLastUpdate();
 }
 
 //Wi-Fi Network Methods
+/*
 void updateWifiNetwork(String wifiNetwork) {
     sendEvent(name: 'wifiNetwork', value: wifiNetwork);
     infoLog("updateWifiNetwork: Device Wi-Fi Network updated, new value = ${wifiNetwork}");
     
     setLastUpdate();
 }
+*/
 
+// Alarm Methods
 def dismissAlarm(String label){
    if(CommandMethod == "Tasker"){ sendTaskerCommand("alarm/dismiss", label); }
    if(CommandMethod == "AutoRemote"){ sendAutoRemoteCommand("dismissAlarm", label); }
+   
+   infoLog("Alarm has been dismissed");
+   debugLog("dismissAlarm: Alarm has been dismissed with label = ${label}");
+   setLastUpdate();
 }
 
 //Device Heartbeat
@@ -351,21 +388,22 @@ def setLastUpdate() {
     sendEvent(name: 'lastUpdated', value : lastUpdate.format('dd/MM/yyyy HH:mm'))
 }
 
+//Tasker and AutoRemote Command Methods
 def sendTaskerCommand(String path, String body){
     try {
         httpPost([uri: "http://${DeviceIPAddress}:${Port}/${mcURLPath()}/${path}", contentType: "application/json", body: "${body}"]) { resp ->
-            debugLog("sendCommand: Command sent.  Response = ${resp.data}")
+            debugLog("sendTaskerCommand: Command sent.  Response = ${resp.data}")
         }
     }
     catch(Exception e) {
-        errorLog("sendCommand: ${e}");
+        errorLog("sendTaskerCommand: ${e}");
     }
 }
 
 def sendAutoRemoteCommand(String command, String value){
     try {
         httpGet([uri: "http://${DeviceIPAddress}:${ARPort}/?message=${command}=:=${java.net.URLEncoder.encode(value)}"]) { resp ->
-            debugLog("sendCommand: Command sent.  Response = ${resp.data}")
+            debugLog("sendAutoRemoteCommand: Command sent.  Response = ${resp.data}")
         }
     }
     catch(Exception e){
